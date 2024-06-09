bb6a2e0196ff477fd303d241c5c15dbe185c326e
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-06-09 18:30:49 +0700

    the App.js as the main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import photo from "./photo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Praktikum React</h1>
+        </div>
+      </nav>
+      <main>
+        <header className="App-header">
+          <img src={photo} className="App-Photo" alt="Gambar" />
+          <h2>Welcome to Praktikum React</h2>
+          <p>
+            Ini adalah hasil Praktikum React
+            <br />{"untuk belajar lebih lanjut, klik link di bawah"}
+          </p>
+          <a
+            className="App-link"
+            href="https://www.youtube.com/watch?v=CgkZ7MvWUAA&pp=ygUIcmVhY3QganM%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React by Youtube
+          </a>
+        </header>
+      </main>
+      <footer className="App-footer">
+        Jodi Nur Farkhani - Pengembangan Desain website
+        <br />
+        &copy; 2024
+      </footer>
     </div>
   );
 }
